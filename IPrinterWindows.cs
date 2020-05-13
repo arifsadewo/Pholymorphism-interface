15dabc56ff2acdc241797978dbf129c97dc40908
istu <istu.2654@students.amikom.ac.id>
2020-05-13 21:26:51 +0700

    Update tugas praktikum 8 dengan interface

---
+++ IPrinterWindows.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_interface
+{
+    interface IPrinterWindows
+    {
+        void Show(PrinterWindows show);
+        void Print(PrinterWindows print);
+    }
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_interface
+{
+    class PrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("\nPrinter display dimension");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Printer printing");
+        }
+    }
+}
